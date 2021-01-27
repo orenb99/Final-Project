93d74d2e2f1c86b747d6a33c9c4a0f43e4ab0944
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-27 16:33:37 +0200

    add to list change
    speperated the divs inside the main container div, added classes and changed the inner divs display to inline

--- src/main.js
+++ src/main.js
@@ -7,7 +7,22 @@ addButton.onclick=addToList;
 function addToList(){
     if(textInput.value!==""){
         let container=document.createElement("div");
-        container.innerText=textInput.value;
+        let itemPriority=document.createElement("div");
+        let itemTime=document.createElement("div");
+        let itemText=document.createElement("div");
+
+        container.classList.add("todo-container");
+        itemPriority.classList.add("todo-priority");
+        itemTime.classList.add("todo-createdAt");
+        itemText.classList.add("todo-text");
+        
+        itemPriority=selector.value+" ";
+        itemTime=new Date()+" ";
+        itemText.innerText=textInput.value;
+        
+        container.append(itemPriority);
+        container.append(itemTime);
+        container.append(itemText);
         viewSection.append(container);
         textInput.value="";
         textInput.focus();
