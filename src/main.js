3e90d3a5a3d2651ced0ea8f320e3c128100ed17c
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-27 15:39:58 +0200

    script
    added the script

---
+++ src/main.js
@@ -0,0 +1,4 @@
+textInput=document.getElementById("text-input");
+addButton=document.getElementById("add-button");
+selector=document.getElementById("priority-selector");
+list=document.getElementById("to-do-list");
