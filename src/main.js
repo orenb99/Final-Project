b5cfb435edbf7ff752c731a37734a6f72882f62b
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-27 15:47:16 +0200

    added onclick function to the addButton
    added an onclick function to the addButton that adds the value of the textInput to the list

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-textInput=document.getElementById("text-input");
-addButton=document.getElementById("add-button");
-selector=document.getElementById("priority-selector");
-list=document.getElementById("to-do-list");
+const textInput=document.getElementById("text-input");
+const addButton=document.getElementById("add-button");
+const selector=document.getElementById("priority-selector");
+const list=document.getElementById("to-do-list");
+
+addButton.onclick=addToList;
+function addToList(){
+    if(textInput.value!==""){
+        let item=document.createElement("li");
+        item.innerText=textInput.value;
+        list.append(item);
+        textInput.value="";
+        textInput.focus();
+    }
+}
