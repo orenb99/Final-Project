e0e0f5f99e072a9b5aa87e52476c887b9e3c91c5
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-02-17 19:11:53 +0200

    basics done

--- backend/api.js
+++ backend/api.js
@@ -1,41 +1,90 @@
-let myTodo=[{
-    id: 1,
-    text:"1",
-    priority:1,
-    date:new Date(),
-    checked:false
-}];
+const fs = require("fs");
+let myTodo=[];
+fs.readdir("./database/", (err, files) => {
+  for(let file of files){
+    myTodo.push(JSON.parse(fs.readFileSync("./database/"+file,"utf8")));
+  }
+});
 
-function addItem(text,priority,date,checked){
-    myTodo.push({
-        id:(myTodo.length+1),
+
+function createItem(text,priority,date,checked){
+   return {
+        id:(myTodo.length),
         text: text,
         priority: priority,
         date: date,
         checked:checked
-    });
+    };
+}
+function updateItem(item,text,priority,date,checked){
+  item.text=text;
+  item.priority=priority;
+  item.date=date;
+  item.checked=checked;
 }
 
-const { response } = require('express');
 const express = require('express');
 const app = express();
 app.use(express.json());
-
 
 app.get("/",(req,res)=>{
     res.send(myTodo);
 })
 
 app.get("/myTodo/:id",(req,res)=>{
-    const id=req.params.id;
-    if(id>myTodo.length||id<=0)
-        res.send("no such id");
+    const {id}= req.params;
+    let intId=parseInt(id);
+    try{
+      res.send(myTodo[intId]);
+    }
+    catch(e){
+      res.send("no item with that id");
+    }
 })
 
-app.put("/",(req,res)=>{
-    res.send(addItem("2",2,new Date(),false));
+app.post("/", (request, response) => {
+    const { body } = request;
+    try {
+      const item=createItem(body.text,body.priority,body.date,body.checked);
+      myTodo.push(item);
+      fs.writeFileSync(
+        `./database/${item.id}.json`,
+        JSON.stringify(item, null, 4)
+      );
+      response.status(201).send(`item ${item.id+1} added`);
+    } catch (e) {
+      response.status(500).json({ message: "Error!", error: e });
+    }
+  });
+
+app.put("/", (request,response)=>{
+  const { body } = request;
+  try{
+    for(let item of myTodo){
+      if(body.id===item.id){
+        updateItem(item,body.text,body.priority,body.date,body.checked);
+        fs.writeFileSync(
+          `./database/${item.id}.json`,
+          JSON.stringify(item, null, 4)
+        );
+        response.send("item updated: \n"+item);
+      }
+    }
+  }
+  catch(e){
+    response.send("no matching id");
+  }
 })
 
 
-
+app.delete("/", (req,res)=>{
+  const {body}= req;
+  try{
+  fs.unlinkSync(`./database/${body.id}.json`);
+  res.send(`item ${body.id} deleted`);
+  }
+  catch{
+    res.send("file doesn't exist");
+  }
+})
 app.listen(3000);
